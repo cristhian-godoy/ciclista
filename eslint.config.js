8cd09f049546c66502bc1599c5ad1d6aa8ae16f7
Cristhian Godoy <thianserpa@gmail.com>
2026-05-24 14:26:45 +0200

    feat: configure package.json linting, enforce sorting and description rules, and automate in pre-commit hook

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import eslintPluginPrettier from 'eslint-plugin-prettier'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import packageJson from 'eslint-plugin-package-json';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -29,5 +30,5 @@ export default defineConfig([
     },
   },
   eslintConfigPrettier,
-])
-
+  packageJson.configs.recommended,
+]);
